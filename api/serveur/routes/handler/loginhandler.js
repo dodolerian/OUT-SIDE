0ce2c51fe0c101ae7e1efce940d296435b5e2050
dorian.martin@i=ynov.comme <dorian.martin@ynov.com>
2024-04-29 16:59:37 +0200

    login done

---
+++ api/serveur/routes/handler/loginhandler.js
@@ -0,0 +1,42 @@
+import { login } from "../../../database/call.js";
+import joi from 'joi';
+
+const valideReq = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+})
+// export async function loginhandler(req, res){
+//   const isValide = valideReq.validate(req.body);
+//   if(isValide.error){
+//     return res.status(400).json({"message":"erreur d'insertion"});
+//   }
+//   const passwordid=login([req.body["email"]]);
+//   const passwordiobj= passwordid[0]
+//   if(req.body["password"]==passwordiobj.password){
+//     return res.status(200).json({"message":"mpd valide go to /home","userid":passwordiobj.id})
+//   }else{
+//     return res.status(400).json({"message":"mdp ou mail incorrect"})
+   
+//   }
+// }
+
+export async function loginhandler(req, res) {
+  const isValide = valideReq.validate(req.body);
+  if (isValide.error) {
+      return res.status(400).json({ "message": "erreur d'insertion" });
+  }
+  
+  try {
+      const passwordid = await login([req.body["email"]]); // Attendre la résolution de la promesse
+      const passwordiobj = passwordid[0];
+
+      if (req.body["password"] === passwordiobj.password) {
+          return res.status(200).json({ "message": "mpd valide go to /home", "userid": passwordiobj.id });
+      } else {
+          return res.status(400).json({ "message": "mdp ou mail incorrect" });
+      }
+  } catch (error) {
+      console.error(error);
+      return res.status(500).json({ "message": "Erreur interne du serveur" });
+  }
+}
--- api/serveur/routes/routes.js
+++ api/serveur/routes/routes.js
@@ -2,10 +2,12 @@ import express from 'express';
 import { testhandler } from './handler/testhandler.js';
 import { registerhandler } from './handler/registerhandler.js';
 import { posthandler } from './handler/posthandler.js';
+import { loginhandler } from './handler/loginhandler.js';
 const router =express.Router();
 
 router.get('/Hello', testhandler);
 router.post('/register', registerhandler);
 router.post('/post', posthandler);
+router.post('/login', loginhandler);
 
 export default router;
